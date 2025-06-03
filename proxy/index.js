09cda71d1651eefedd98e19a82f11666a885c5db
Maksym Syvak <88047057+Mixam07@users.noreply.github.com>
2025-06-03 22:34:44 +0300

    Update index.js

--- proxy/index.js
+++ proxy/index.js
@@ -1,7 +1,19 @@
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
-export default createProxyMiddleware({
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+const proxy = createProxyMiddleware({
   target: 'http://209.38.195.221:3000',
   changeOrigin: true,
-  pathRewrite: { '^/api': '' },
+  pathRewrite: {
+    '^/api': ''
+  }
 });
+
+export default function handler(req, res) {
+  return proxy(req, res, () => {});
+}
